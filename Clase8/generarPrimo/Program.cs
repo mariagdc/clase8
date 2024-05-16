e4d3b37f63471d683a5fe0afcc64a47ef21f5dd0
mariagdc <mariagodoydelcastillo@gmail.com>
2024-05-16 17:55:07 -0300

    Ejercicio con metodooos

--- Clase8/generarPrimo/Program.cs
+++ Clase8/generarPrimo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace generarPrimo
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Ingrese un numero");
+            string varText = (Console.ReadLine());
+            int num = Convert.ToInt32(varText);
+            #region contador divisibles;
+            int divisible = 0;
+            for (int n = 1; n <= num; n++) ;
+            {
+                if (num % n == 0)
+                {
+                    divisible++;
+
+                }
+            #endregion
+
+                if (divisible <= 2)
+                {
+                    Console.WriteLine("El numero es primo");
+                }
+                else
+                {
+                    Console.WriteLine("El numero no es primo");
+
+                }
+
+            }
         }
     }
 }
--- Clase8/verificarPrimo/Program.cs
+++ Clase8/verificarPrimo/Program.cs
@@ -10,6 +10,38 @@ namespace verificarPrimo
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Ingres desde que numero y hasta que numero");
+            int desde = Convert.ToInt32(Console.ReadLine());
+            int hasta = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = desde; i <=hasta; i++)
+            {
+                //evaluar si es primo
+                int numero = n;
+
+                #region contador divisibles;
+                int divisible = 0;
+                for (int n = 1; n <= num; n++) ;
+                {
+                    if (num % n == 0)
+                    {
+                        divisible++;
+
+                    }
+                    #endregion
+                }
+                }
+            bool esPrimo = false;
+            if (divisible <= 2)
+            {
+                Console.WriteLine("El numero es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es primo");
+
+            }
+            
+            if ()
     }
 }
